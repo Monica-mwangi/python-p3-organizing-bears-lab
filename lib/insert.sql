f860eac07418d218a243bfcf4f2b645f2d419cc1
Monica <monica@DESKTOP-A4KS9MU.localdomain>
2024-03-11 18:04:01 -0700

    done with the lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 5, 'M', 'Yellow', 'Playful', 1),
+  ('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+  ('Tabitha', 9, 'F', 'Gray', 'Calm', 1),
+  ('Sergeant Brown', 7, 'M', 'Brown', 'Brave', 1),
+  ('Melissa', 7, 'F', 'Golden', 'Friendly', 1),
+  ('Grinch', 6, 'M', 'Green', 'Sly', 1),
+  ('Wendy', 4, 'F', 'White', 'Sweet', 1),
+  (NULL, NULL, NULL, NULL, NULL, NULL);
